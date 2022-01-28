ec072791df85b040a32223b047abe225ef2b8220
MdFahad012 <97295844+MdFahad012@users.noreply.github.com>
2022-01-29 02:27:32 +0530

    Port fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server has started successfully");
+});
